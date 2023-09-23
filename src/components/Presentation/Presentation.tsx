9f920f8b62490995dea55ede6e77a89367917aa3
Ekhi <e.arzak04@gmail.com>
2023-09-23 20:28:55 +0200

    FINAL VERSION

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -5,7 +5,7 @@ export const Presentation = () => {
                 Cache <span className="bg-yellow-100 text-yellow-800 text-2xl font-extrabold mr-2 px-2.5 py-0.5 rounded ">SIM</span>
             </h1>
             <p className="flex justify-center items-center relative font-light">
-                Simulador de Cache simple hecho con React + Typescript. &nbsp; <a href="#" className="font-medium text-blue-600 underline">Ver repositorio en GitHub</a>
+                Simulador de Cache simple hecho con React + Typescript. &nbsp; <a href="https://github.com/ekhi-arzac/cache-simulator/" className="font-medium text-blue-600 underline">Ver repositorio en GitHub</a>
             </p>
         </>
 
